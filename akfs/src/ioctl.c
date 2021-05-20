94034e6c6809eadf9f3a677c661daee1fd18162a
phil-fly <dpf891x@163.com>
2021-05-20 15:20:16 +0800

    添加子模块

--- akfs/src/ioctl.c
+++
@@ -1,9 +0,0 @@
-#include "akfs.h"
-
-/**
- * @brief akfs_get_mlen 
- *   获取映射长度接口
- */
-int akfs_get_mlen(akfs_args_t *args_ptr ,unsigned long args){
-    return copy_to_user((void __user *)args ,&args_ptr->rlen ,sizeof(unsigned int));
-}
