d802ded60772a1f6f372e3c031466f51a5f3f7f3
phil-fly <712127467@qq.com>
2021-07-20 14:27:41 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+version="v1.0.1"
+akagentDockerPkg="akagent-docker-${version}.tar"
+
+
 install_akfs(){
     insmod akfs.ko
     test -D /opt/mount||mkdir -p /opt/mount
@@ -18,12 +22,50 @@ install_aknet(){
     insmod net.ko
 }
 
-install_akagent(){
-    docker build -t bxsec/akagent:v1.0.1 .
+install_akagent_docker(){
+    docker load -i ${akagentDockerPkg}
 }
 
+install_akagent_service(){
+    docker load -i ${akagentDockerPkg}
+}
+
+
+function person_menu(){  
+cat << EOF
+==================================================================
+                        百晓安装服务
+                                                by phil-fly
+==================================================================
+`echo -e "\033[35m 1. 安装docker模式\033[0m"`
+`echo -e "\033[35m 2. 安装service模式\033[0m"`
+
+                                                        
+userspace program 安装方式选择: 1/2
+EOF
+read -p "输入选择：" num2
+case $num2 in
+ 1)
+  echo -e "\033[35m 开始docker模式安装.\033[0m"
+  install_akagent_docker
+  ;;
+ 2)
+  echo -e "\033[35m 开始本地service模式安装.\033[0m"
+  echo -e "\033[35m 暂不支持.\033[0m"
+  ;;
+ *)
+  echo "the is Fail!!"
+  person_menu
+esac
+}
+
+
+person_menu
 install_akfs
 install_akps
 install_akfile
 install_aknet
-install_akagent
+
+
+
+
